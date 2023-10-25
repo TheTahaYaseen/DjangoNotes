4d96b7228ae9c67e94aa72f23f8811c83bbd2d74
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-25 12:07:34 +0500

    Basic Idea Of Django

---
+++ Notes.py
@@ -0,0 +1,29 @@
+"""
+
+Django:
+
+- Python Web Framework
+    Package To Assist And Speed Web Development In This Case
+
+- Server Side Framework
+    While Frontend Framework Helps In he Frontend Aspect, Backend Handles All The Server Side Stuff For Making A Site Functional
+
+- Desgined To Get You Started Fast
+    Google, Youtube, Instagram, Pinterest, Udemy, Dropbox, NASA, Onion, Reddit, Eventbrite, Robinhood And More Use Django As Their Backend
+
+- Batteries Included
+    Comes With All The Functional Components Within The Box (Package)
+
+- MVT Design Patter
+
+    Like MVC But The Controller Aspect Is Taken Care Of By The Framework
+
+    Model -  Data Access Layer - Data Related Operations Handler 
+    Template - Presentation Layer - User's View
+    View - Business Logic - Presenting The Data To The User Dynamically
+
+Like Laravel Or Ruby On Rails
+
+Flask Is Good But Doesn't Have That Great Of A Structure.
+
+"""
